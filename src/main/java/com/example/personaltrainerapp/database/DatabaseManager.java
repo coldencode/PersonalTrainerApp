6e6c1a731793620e12a8e27c361cf4f49b25c522
ccho0117 <ccho0117@student.monash.edu>
2026-05-03 21:30:55 +1000

    Added documentation for DatabaseManager.java

--- src/main/java/com/example/personaltrainerapp/database/DatabaseManager.java
+++ src/main/java/com/example/personaltrainerapp/database/DatabaseManager.java
@@ -5,10 +5,20 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Logger;
 
+/**
+ *  Sets up the connection for the database
+ */
 public class DatabaseManager {
+    /**
+     * JDBC Connection URL for the SQLite Database
+     */
     private static final String URL = "jdbc:sqlite:personaltrainer.db";
     private final Logger logger = Logger.getLogger(this.getClass().getName());
 
+    /**
+     * Establishes and returns a new connection URL to the database.
+     * @return Connection - Connection URL to the database
+     */
     public Connection getConnection() {
         try {
 //            if(connection == null || connection.isClosed()) {
